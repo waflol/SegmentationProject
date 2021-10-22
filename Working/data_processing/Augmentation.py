2bed20512d5bcf4a8f9792314fde19b88208967f
waflol <lenguyenhunganh2001@gmail.com>
2021-10-23 04:34:53 +0700

    Update Augmentation.py

--- Working/data_processing/Augmentation.py
+++ Working/data_processing/Augmentation.py
@@ -11,8 +11,8 @@ def get_training_augmentation():
 
         A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
 
-        A.PadIfNeeded(min_height=256, min_width=256, always_apply=True, border_mode=0),
-        A.RandomCrop(height=256, width=256, always_apply=True),
+        A.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
+        A.RandomCrop(height=320, width=320, always_apply=True),
         A.augmentations.transforms.GaussNoise(p=0.2),
         #A.IAAAdditiveGaussianNoise(p=0.2),
         #A.IAAPerspective(p=0.5),
